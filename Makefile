7c8e7b02b5eedf2044664afde0f23195688ff663
min.du <min.du@horizon.ai>
2019-07-07 09:26:49 +0800

    update

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
-init:
-	pip3 install -r requirements.txt
+install:
+	python3.6 setup.py install
+
+uninstall:
+	python3.6 setup.py install --record install.log
+	cat install.log | xargs rm -rf 
+	rm install.log
 
 test:
 	python3.6 /usr/bin/nosetests -s tests
--- setup.py
+++ setup.py
@@ -1,28 +1,40 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import io
+import re
 
-with open('README.md') as f:
+with open('README.md', 'r') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open('LICENSE', 'r') as f:
     license = f.read()
 
-with io.open("samplemod/__init__.py", "rt", encoding="utf8") as f:
-    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+with open('requirements.txt', 'r') as f:
+    requires = []
+    for line in f.readline():
+        line = line.strip()
+        if not line.startswith('#'):
+            requires.append(line)
+    __import__('ipdb').set_trace()
+
+
+with io.open("src/sample/__init__.py", "rt", encoding="utf8") as f:
+    version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 
 setup(
-    name='samplemod',
+    name='sample',
     version=version,
     description='TODO',
     long_description=readme,
-    author='Duino Du',
+    author='user',
     author_email='duino472365351@gmail.com',
-    url='https://github.com/duinodu/samplemod',
+    url='https://github.com/user/sample',
     license=license,
-    packages=find_packages("samplemod")
+    packages=find_packages("src"),
+    package_dir={'': 'src'},
     include_package_data=True,
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=[],
-    entry_points={"console_scripts": ["samplemod = samplemod.cli:main"]},
+    entry_points={"console_scripts": ["sample = sample.cli:main"]},
 )
